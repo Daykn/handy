221a68831328c4ff49b072937a1d4bab9c7cbbbb
Dauren Ordabayev <ordabayevdauren@gmail.com>
2018-10-26 05:22:52 +0600

    made information cards about directors

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,8 +1,18 @@
 <link rel="stylesheet" href="{{asset('/css/footer.css')}}">
 
-<footer class="footer col-md-12" style="height:350px; background:#00a1ba;">
+<footer class="footer col-md-12" style=" background:#4a76a8;">
 <div class="container-fluid">
-<form action="/action_page.php " class="col-md-6 col-md-offset-5">
+    
+  <div class="contact col-md-6" style="text-align:start;">
+    <h2>Контакты</h2>   
+    
+  <h4> Телефон</h4>
+      <p style="color:white;">- 8 777 77 77</p>
+      <p style="color:white;">- 8 444 44 44</p>
+  </div>
+
+<form action="/action_page.php " class="col-md-4 col-md-offset-2">
+<label><h2 >Обратная Связь</h2></label>
     <label for="fname">Имя</label>
     <input type="text" id="fname" name="firstname" placeholder="Ваше имя">
 
@@ -10,7 +20,7 @@
     <input type="text" id="lname" name="lastname" placeholder="Ваш email">
 
     <label for="subject">Отзывы / Пожелания</label>
-    <textarea id="subject" name="subject" placeholder="Обратная связь" style="height:100px;width:270px;"></textarea>
+    <textarea id="subject" name="subject" placeholder="Обратная связь" style="height:70px;width:300px;"></textarea>
 
     <input type="submit" value="Submit">
   </form>
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="{{asset('/css/nav.css')}}">
 
-<nav class="navbar " style="background:#00a1ba;">
+<nav class="navbar" style="background:#4a76a8;">
   <div class="container-fluid col-md-12">
     <div class="navbar-header col-md-2">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -8,7 +8,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>                        
       </button>
-      <a class="navbar-brand" href="#">WebSiteName</a>
+      <a class="navbar-brand" href="#" style="color:white;">RELIANCE</a>
     </div>
    
     <div  class="collapse navbar-collapse " id="myNavbar">
@@ -17,7 +17,7 @@
         <li class="dropdown col-md-offset-1">
           <a class="dropdown-toggle" id="li" data-toggle="dropdown" href="#" >Новости<span class="caret"></span></a>
           <ul class="dropdown-menu" style="background:#eee;">
-            <li><a href="#" id="drop">Основные</a></li>
+            <li><a href="{{ asset('\resources\views\news.blade.php') }}" id="drop">Основные</a></li>
             <li><a href="#" id="drop">Проекты</a></li>
             <li><a href="#" id="drop">Тренинги и семинары</a></li>
             <li><a href="#" id="drop">Волонтерская деятельность</a></li>
@@ -26,10 +26,10 @@
           </ul>
         </li>
         <li class="col-md-offset-1"><a href="#" id="li">Отчеты</a></li>
-        <li class="col-md-offset-1"><a href="#" id="li">Контакты</a></li>
+        <li class="col-md-offset-1"><a href="" id="li">Контакты</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        <li ><a href="#" style="color:white;"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
       </ul>
     </div>
 
